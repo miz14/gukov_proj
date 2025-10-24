549d648f4980afe7ede6801f70de74cc1887b3d7
miz14 <vpashenko14@gmail.com>
2025-10-24 18:43:03 +0600

    test-adding-ssl 2

--- reverse-proxy/ssl-magager.sh
+++ reverse-proxy/ssl-magager.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SSL_DIR="/etc/letsencrypt"
 LIVE_DIR="$SSL_DIR/live"
-DOMAINS=("${SITE1_DOMAIN}" "${SITE2_DOMAIN}" "www.${SITE1_DOMAIN}" "www.${SITE2_DOMAIN}")
+DOMAINS="${SITE1_DOMAIN}" "${SITE2_DOMAIN}" "www.${SITE1_DOMAIN}" "www.${SITE2_DOMAIN}"
 
 check_nginx_config() {
     echo "Проверка конфигурации nginx..."
