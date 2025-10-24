813eec95b5cd3c58ee1206caff13a6186436f551
miz14 <vpashenko14@gmail.com>
2025-10-24 19:19:07 +0600

    test-adding-ssl 7

--- reverse-proxy/ssl-magager.sh
+++ reverse-proxy/ssl-magager.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 SSL_DIR="/etc/letsencrypt"
 LIVE_DIR="$SSL_DIR/live"
 DOMAINS="${SITE1_DOMAIN} ${SITE2_DOMAIN} www.${SITE1_DOMAIN} www.${SITE2_DOMAIN}"
--- reverse-proxy/test.sh
+++
@@ -1,9 +0,0 @@
-SSL_DIR="/etc/letsencrypt"
-LIVE_DIR="$SSL_DIR/live"
-DOMAINS="${SITE1_DOMAIN} ${SITE2_DOMAIN} www.${SITE1_DOMAIN} www.${SITE2_DOMAIN}"
-
-main () {
-    echo "=== test ==="
-}
-
-main
