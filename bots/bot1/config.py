f8331ef55e4667bbeed8c34c514dd2fb70aa82a8
miz14 <vpashenko14@gmail.com>
2025-10-27 17:16:22 +0600

    убрал открытый токен после тестов

--- bots/bot1/config.py
+++ bots/bot1/config.py
@@ -1,6 +1,5 @@
 import os
 
-# BOT_TOKEN = os.getenv('BOT1_TOKEN') 
-BOT_TOKEN = '8153581204:AAEyrcp-vu2mO35a8qJbu-XYKpHfPhHwaUk'
+BOT_TOKEN = os.getenv('BOT1_TOKEN') 
 GOOGLE_POST_URL = os.getenv('GOOGLE_POST_URL')
 BOT_SERVICE = 'Юридические услуги в сфере пенсионного права'
